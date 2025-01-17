84e45e96f86fd7208edf41ff9d5ce594fe9c918c
siem <siemvahoof@gmail.com>
2025-01-17 15:16:01 +0100

    ee

--- database/seeders/PizzaSizeSeeder.php
+++ database/seeders/PizzaSizeSeeder.php
@@ -2,22 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\PizzaSize;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
-class pizzasizeseeder extends Seeder
+class PizzaSizeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('pizza_sizes')->insert([
-            ['pizza_id' => 1, 'size' => 'small', 'price' => 8.99],
-            ['pizza_id' => 1, 'size' => 'medium', 'price' => 10.99],
-            ['pizza_id' => 1, 'size' => 'large', 'price' => 12.99],
-        ]);
+        PizzaSize::create(['pizza_id' => 1, 'size' => 'small', 'price' => 8.99]);
+        PizzaSize::create( ['pizza_id' => 1, 'size' => 'medium', 'price' => 10.99]);
+        PizzaSize::create(['pizza_id' => 1, 'size' => 'large', 'price' => 12.99]);
         
     }
 }
